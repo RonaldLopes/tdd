f876ac201733362b0608e01d92a325b95ea9bc3d
ronald.lopes <ronald.s.lopes96@gmail.com>
2019-09-19 10:48:20 -0300

    Upload inicial

---
+++ Testes/TesteSerial.py
@@ -0,0 +1,21 @@
+
+import unittest
+
+from should_dsl import should
+
+
+
+
+class TesteSerial(unittest.TestCase):
+
+    def __init__(self):
+        pass
+
+    def teste_ler_serial(self):
+        pass
+
+    def teste_processa_dados_brutos(self):
+        pass
+
+    def teste_obter_serial(self):
+        pass
